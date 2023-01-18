193442df51df7e579c4604c316da28fc64537f7e
Elliott Denis <elliottdenis@hotmail.fr>
2023-01-18 15:42:25 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,59 @@
-import * as React from "react"
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
   VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Card,
+  Stack,
+  Heading,
+  CardBody,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Flex,
+  CardHeader,
+  CardFooter,
+  Container,
+} from '@chakra-ui/react'
+import ColorModeSwitcher from './components/ColorModeSwitcher'
+import ConnectWallet from './components/ConnectWallet'
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export default function App() {
+  return (
+    <VStack minH="100vh">
+      <ColorModeSwitcher ml="auto" />
+      <Container maxW="xl" centerContent>
+        <Card shadow="xl" w="full">
+          <CardHeader>
+            <Flex justify="space-between" align="baseline">
+              <Heading as="h1" fontSize="2xl" letterSpacing="wide">
+                Wallet
+              </Heading>
+              <ConnectWallet />
+            </Flex>
+          </CardHeader>
+          <form>
+            <CardBody>
+              <Stack w="sm" mx="auto" spacing="6">
+                <Heading as="h2" fontWeight="normal" fontSize="xl">
+                  Send ETH
+                </Heading>
+                <FormControl>
+                  <FormLabel size="xl">Amount</FormLabel>
+                  <Input type="text" borderRadius="6px" />
+                </FormControl>
+                <FormControl>
+                  <FormLabel size="sm">Address</FormLabel>
+                  <Input type="text" borderRadius="6px" />
+                </FormControl>
+              </Stack>
+            </CardBody>
+            <CardFooter>
+              <Button colorScheme="orange" size="lg" w="sm" mx="auto">
+                Send
+              </Button>
+            </CardFooter>
+          </form>
+        </Card>
+      </Container>
+    </VStack>
+  )
+}
---
+++ src/components/ConnectWallet.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+const ConnectWallet: React.FC = (props) => {
+  return <Button>Connect Wallet</Button>
+}
+
+export default ConnectWallet
